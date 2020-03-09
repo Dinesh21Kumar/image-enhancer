fe4cb3c8d5689861de3fba3e9238b0feb2c78baa
Dinesh Kumar <dinesh21.kumar@ril.com>
2020-03-09 16:02:20 +0530

    updated init

--- __init__.py
+++ __init__.py
@@ -1,17 +1,33 @@
+from builtins import int
+
 import cv2
+import numpy as np
 import tf
 import evaluation as ev
+import os
 
+cwd = os.getcwd()
+files = os.listdir(cwd + "/static/uploads")
+inputFolder = cwd + "/static/uploads"
+outputFolder = cwd + "/static/result"
+compFolder = cwd + "/static/compare"
+for f in files:
+    print("f = " + f)
+    file_name_without_ext = f.split(".")[0]
+    outputFileName = file_name_without_ext + "_enhanced"
+    f = tf.getInputPhoto(f)
+    tf.processImg(f, outputFileName)
+    img1 = cv2.imread(inputFolder + "/" + f)
+    img2 = cv2.imread(outputFolder + "/" + outputFileName + ".png")
+    vis = np.concatenate((img1, img2), axis=1)
+    cv2.imwrite(os.path.join(compFolder, file_name_without_ext + "_vs_" + file_name_without_ext + "_enhanced.jpg"), vis)
 
-tf.getInputPhoto('input.png')
-tf.processImg('input.png', 'output')
+# inp = cv2.imread('static/uploads/input.png', 1)
+# outp = cv2.imread('static/result/output.png', 1)
 
-inp = cv2.imread('static/uploads/input.png', 1)
-outp = cv2.imread('static/result/output.png', 1)
-
-cv2.imshow('Input', inp)
-cv2.imshow('Output', outp)
-cv2.waitKey()
+# cv2.imshow('Input', inp)
+# cv2.imshow('Output', outp)
+# cv2.waitKey()
 
 '''
 inp = cv2.imread('static/result-db/1a.png', 1)
